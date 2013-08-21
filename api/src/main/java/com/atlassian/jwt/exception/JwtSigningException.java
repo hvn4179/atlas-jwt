f1adc7d745e2add9e54b18624bd7fb4395931010
Tim Pettersen <tim@atlassian.com>
2013-08-21 14:00:36 -0700

    JwtSigningException now extends RuntimeException

--- api/src/main/java/com/atlassian/jwt/exception/JwtSigningException.java
+++ api/src/main/java/com/atlassian/jwt/exception/JwtSigningException.java
@@ -5,7 +5,7 @@ package com.atlassian.jwt.exception;
  *
  * @since 1.0
  */
-public class JwtSigningException extends Exception
+public class JwtSigningException extends RuntimeException
 {
     public JwtSigningException(Exception cause)
     {
