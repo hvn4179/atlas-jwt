ac58d3b6877eafd6da8567cb6b0f8032088a1f91
Peter Brownlow <pbrownlow@atlassian.com>
2013-09-11 10:53:23 +1000

    corrected javadoc

--- api/src/main/java/com/atlassian/jwt/reader/JwtClaimVerifier.java
+++ api/src/main/java/com/atlassian/jwt/reader/JwtClaimVerifier.java
@@ -2,7 +2,16 @@ package com.atlassian.jwt.reader;
 
 import com.atlassian.jwt.exception.JwtVerificationException;
 
+/**
+ * Interface for asserting that a JWT claim is valid.
+ * Implementations include straightforward equality checking and verification of signatures.
+ */
 public interface JwtClaimVerifier
 {
+    /**
+     * Assert that the claimed value is valid.
+     * @param claim The value of the JWT claim.
+     * @throws JwtVerificationException if the claim is invalid or could not be verified
+     */
     public void verify(Object claim) throws JwtVerificationException;
 }
