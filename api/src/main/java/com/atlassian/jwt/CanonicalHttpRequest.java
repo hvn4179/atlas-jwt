df8e0fbc0a37117e92d8bcb33ca2aff7285235d0
Peter Brownlow <pbrownlow@atlassian.com>
2013-09-24 09:03:34 +1000

    added javadoc

--- api/src/main/java/com/atlassian/jwt/CanonicalHttpRequest.java
+++ api/src/main/java/com/atlassian/jwt/CanonicalHttpRequest.java
@@ -9,8 +9,29 @@ import java.util.Map;
  */
 public interface CanonicalHttpRequest
 {
+    /**
+     * HTTP method (e.g. "GET", "POST" etc).
+     * @return the HTTP method in upper-case.
+     */
     public String getMethod();
+
+    /**
+     * The part of an absolute URL that is after the protocol, server, port and context path.
+     * E.g. "/the_uri" in "http://server:80/context/the_uri?param=value".
+     * @return the relative URI with no case manipulation.
+     */
     public String getUri();
+
+    /**
+     * The part of an absolute URL that has been added by a reverse proxy, or "/" if no such redirection has occurred.
+     * E.g. "/context" after "http://subdomain.server:80/the_uri?param=value" was redirected to "http://server:80/context/the_uri?param=value".
+     * @return the context path with no case manipulation.
+     */
     public String getContextPath();
+
+    /**
+     * The {@link Map} of parameter-name to parameter-values.
+     * @return {@link Map} representing { parameter-name-1 to { parameter-value-1, parameter-value-2 ... }, parameter-name-2 to { ... }, ... }
+     */
     public Map<String, String[]> getParameterMap();
 }
