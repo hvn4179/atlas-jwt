8cfdef3c075b5de50799eea5990bb0414135b3e9
Peter Brownlow <pbrownlow@atlassian.com>
2013-10-10 13:14:53 +1100

    clarified javadoc for JwtUtil.JWT_AUTH_HEADER_PREFIX

--- core/src/main/java/com/atlassian/jwt/core/JwtUtil.java
+++ core/src/main/java/com/atlassian/jwt/core/JwtUtil.java
@@ -18,7 +18,11 @@ public class JwtUtil
     public static final String AUTHORIZATION_HEADER = "Authorization";
 
     /**
-     * The start of a valid Authorization header specifying a JWT message.
+     * The start of a valid Authorization header specifying a JWT message.<p>
+     * Note the space at the end of the prefix; the header's format is:
+     *  <pre>{@code
+     *      JwtUtil.JWT_AUTH_HEADER_PREFIX + "<insert jwt message here>"
+     *  }</pre>
      */
     public static final String JWT_AUTH_HEADER_PREFIX = "JWT ";
 
