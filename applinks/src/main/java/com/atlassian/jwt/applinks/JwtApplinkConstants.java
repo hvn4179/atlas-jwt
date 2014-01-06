384b1fba4cc050af9c80b563a8d524ac63402aca
Peter Brownlow <pbrownlow@atlassian.com>
2014-01-06 11:57:42 +1100

    ACDEV-823: corrected JWT_AUTH_METHOD value to "JWT"

--- applinks/src/main/java/com/atlassian/jwt/applinks/JwtApplinkConstants.java
+++ applinks/src/main/java/com/atlassian/jwt/applinks/JwtApplinkConstants.java
@@ -4,5 +4,5 @@ public class JwtApplinkConstants
 {
     public static final String PLUGIN_KEY_PROPERTY = "plugin-key"; // TODO: ACDEV-663: also defined in atlassian-connect's DefaultConnectApplinkManager
     public static final String AUTH_METHOD_PROPERTY = "atlassian.auth.method"; // TODO: ACDEV-663: share AuthenticationMethod with atlassian-connect's DefaultConnectApplinkManager
-    public static final String JWT_AUTH_METHOD = "JWT_AUTH_METHOD"; // TODO: ACDEV-663: share AuthenticationMethod with atlassian-connect's DefaultConnectApplinkManager
+    public static final String JWT_AUTH_METHOD = "JWT"; // TODO: ACDEV-663: share AuthenticationMethod with atlassian-connect's DefaultConnectApplinkManager
 }
