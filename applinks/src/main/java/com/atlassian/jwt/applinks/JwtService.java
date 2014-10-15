abebde1d86c204400c0546f81891c2cfe15491f3
Peter Brownlow <pbrownlow@atlassian.com>
2014-10-15 16:43:41 +1100

    AC-1356: JWT issuing can be done with a secret (as opposed to an app link from which we then look up the secret)

--- applinks/src/main/java/com/atlassian/jwt/applinks/JwtService.java
+++ applinks/src/main/java/com/atlassian/jwt/applinks/JwtService.java
@@ -5,7 +5,11 @@ import com.atlassian.applinks.api.ApplicationType;
 import com.atlassian.applinks.api.TypeNotInstalledException;
 import com.atlassian.jwt.Jwt;
 import com.atlassian.jwt.applinks.exception.NotAJwtPeerException;
-import com.atlassian.jwt.exception.*;
+import com.atlassian.jwt.exception.JwtIssuerLacksSharedSecretException;
+import com.atlassian.jwt.exception.JwtParseException;
+import com.atlassian.jwt.exception.JwtSigningException;
+import com.atlassian.jwt.exception.JwtUnknownIssuerException;
+import com.atlassian.jwt.exception.JwtVerificationException;
 import com.atlassian.jwt.reader.JwtClaimVerifier;
 
 import java.util.Map;
@@ -54,6 +58,23 @@ public interface JwtService
      * @throws NotAJwtPeerException if this server does not have a JWT relationship with the
      *                              {@link ApplicationLink linked application} indicated in the JWT.
      * @throws JwtSigningException  if a problem was encountered while generating the JWT
+     *
+     * @deprecated migrate to {@link #issueJwt(String, String)}
      */
+    @Deprecated
     String issueJwt(String jsonPayload, ApplicationLink applicationLink) throws NotAJwtPeerException, JwtSigningException;
+
+    /**
+     * Generate a JWT for the supplied payload, suitable for authenticating with the specified
+     * {@link ApplicationLink linked application}. Use {@link #isJwtPeer(ApplicationLink)} to check whether the
+     * {@link ApplicationLink linked application} is a bone fide JWT peer before calling this method.
+     *
+     * @param jsonPayload     a JSON payload
+     * @param secret the shared-secret or private-key used to sign
+     * @return the JWT
+     * @throws NotAJwtPeerException if this server does not have a JWT relationship with the
+     *                              {@link ApplicationLink linked application} indicated in the JWT.
+     * @throws JwtSigningException  if a problem was encountered while generating the JWT
+     */
+    String issueJwt(String jsonPayload, String secret) throws NotAJwtPeerException, JwtSigningException;
 }
